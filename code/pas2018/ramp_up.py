a43f5b1c247cad75058f1d054054deb9cd45b350
Onjyoji-Toki <155806712+Onjyoji-Toki@users.noreply.github.com>
2024-02-09 18:18:56 +0900

    Delete code directory

--- code/pas2018/ramp_up.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from .current_controller import CurrentController
-
-def ramp_up():
-    c_con = CurrentController()
-    c_con.ramp_from_to(0, 8, wait=0.2)
--- code/wavedump.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-def wavedump():
-    subprocess.run(r'C:\Users\kyoto\digitizer\test4.exe 300', shell=True)
